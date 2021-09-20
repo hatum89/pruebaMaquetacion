f4016e2ab2a3cc6c843ad49e68882ea6717a30f5
juan <juanhatumvarela@gmail.com>
2021-09-19 21:13:24 -0500

    vamos bien con fe

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {SliderInterface} from '../../interfaces/slider-interface';
 import {SLIDER_DATA_ITEMS} from '../../constants/img.constant';
@@ -9,6 +9,12 @@ import {SLIDER_DATA_ITEMS} from '../../constants/img.constant';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
+  @Input() height = 700;
+  @Input() isFullScreen = false;
+  @Input() items: SliderInterface[] = [];
+
+  public finalHeight: string | number = 0;
+  public currentPosition = 0;
   public sliderData: SliderInterface[] = SLIDER_DATA_ITEMS;
   public products = [
     {
@@ -46,6 +52,7 @@ export class BodyComponent implements OnInit {
   public emailMessageShow: boolean;
   arr1 = ['a', 'b', 'c', 'd'];
   constructor( private formBuilder: FormBuilder) {
+    this.finalHeight = this.isFullScreen ? '100vh' : `${this.height}px`;
     this.showMessage1 = false;
     this.showMessage2 = false;
     this.emailMessageShow = false;
@@ -58,6 +65,10 @@ export class BodyComponent implements OnInit {
 
   ngOnInit(): void {
     this.shuffleArray();
+    this.items.map( ( i, index ) => {
+      i.id = index;
+      i.marginLeft = 0;
+    });
   }
 
   save(): void {
@@ -92,5 +103,33 @@ export class BodyComponent implements OnInit {
   }
   shuffleArray(): void{
     this.arr1.sort(() => Math.random() - 0.5);
+  }
+  setCurrentPosition(position: number): void {
+    this.currentPosition = position;
+    this.items.find(i => i.id === 0).marginLeft = -100 * position;
+  }
+  setNext(): void{
+    let finalPercentage = 0;
+    let nextPosition = this.currentPosition + 1;
+    if (nextPosition <= this.items.length - 1) {
+      finalPercentage = -100 * nextPosition;
+    } else {
+      nextPosition = 0;
+    }
+    this.items.find(i => i.id === 0).marginLeft = finalPercentage;
+    this.currentPosition = nextPosition;
+  }
+  setBack(): void {
+    let finalPercentage = 0;
+    let backPosition = this.currentPosition  - 1;
+    if (backPosition >= 0) {
+      finalPercentage = -100 * backPosition;
+    } else {
+      backPosition = this.items.length - 1;
+      finalPercentage = -100 * backPosition;
+    }
+    this.items.find(i => i.id === 0).marginLeft = finalPercentage;
+    this.currentPosition = backPosition;
+
   }
 }
