290d08cc294a6ca1a5b3773f99f0cef5fb3af840
juan <juanhatumvarela@gmail.com>
2021-09-21 06:08:22 -0500

    a la de Dios ccon fe

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {getAttributeValue} from 'codelyzer/util/getAttributeValue';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +12,12 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  menuButton(): void{
+    const iconMenu = document.querySelector('#menu-icon');
+    const menu = document.querySelector('#menu');
+    iconMenu.addEventListener('click', (e) => {
+      menu.classList.toggle('active');
+      document.body.classList.toggle('opacity');
+    });
+  }
 }
